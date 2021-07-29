193826cb1bbbb3568c3f6b258b8117fc8940d397
Denis Makouski (Reiko) <denis.makouski@bseu.by>
2021-07-29 11:58:24 +0300

    version ++

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021072700; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021072900; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'report_averagechecktime'; // Full name of the plugin (used for diagnostics).
 
